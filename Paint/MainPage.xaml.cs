bbfeae4794e9a41856d252e824b9ad43c84fefb4
Czezu <54447101+Czezu@users.noreply.github.com>
2025-03-31 18:51:34 +0200

    Add files via upload

---
+++ Paint/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Paint
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void poleRysowania_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void poleRysowania_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void poleRysowania_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+    }
+}
